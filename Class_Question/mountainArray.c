1ec25fdf683e1e9853dab922a134da1eb112aca5
akshay_dev <akshayrishu4@gmail.com>
2026-02-20 22:05:05 +0530

    update

--- Class_Question/mountainArray.c
+++ Class_Question/mountainArray.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
-// void swap(int arr[], int i, int j)
-// {
-//     printf("swaping %d and %d\n", arr[i], arr[j]);
-//     int temp = arr[i];
-//     arr[i] = arr[j];
-//     arr[j] = temp;
-//     int n = sizeof(arr) / sizeof(arr[0]);
-//     printf("Size %d\n", n);
-//     for (int k = 0; k < n; k++)
-//     {
-//         printf("%d ", arr[k]);
-//     }
-//     printf("\n");
-// }
 int main()
 {
     int arr[] = {9, 10, 11, 12, 13, 5};
@@ -36,52 +22,6 @@ int main()
         printf("%d ", arr[i]);
     }
     printf("\n");
-
-    // int s = 0;
-    // int e = n - 1;
-
-    // while (s < e)
-    // {
-    //     // printf("checking %d and %d\n",  arr[s], arr[e]);
-    //     if (arr[s] > arr[e])
-    //     {
-    //         int temp = arr[s];
-    //         arr[s] = arr[e];
-    //         arr[e] = temp;
-    //         for (int i = 0; i < n; i++)
-    //         {
-    //             printf("%d ", arr[i]);
-    //         }
-    //         printf("\n");
-    //         if (s <= peakIndex)
-    //         {
-    //             s++;
-    //         }
-    //         else
-    //         {
-    //             e--;
-    //         }
-    //     }
-    //     if (arr[s] < arr[e])
-    //     {
-    //         if (s <= peakIndex)
-    //         {
-    //             s++;
-    //         }
-    //         else
-    //         {
-    //             e--;
-    //         }
-    //     }
-    //     printf("S: %d, E: %d\n", s, e);
-
-    // }
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     printf("%d ", arr[i]);
-    // }
-    // printf("\n");
 
     return 0;
 }
